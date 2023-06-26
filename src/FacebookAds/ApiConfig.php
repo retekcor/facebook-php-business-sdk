2d45dfa702c0244e532736d8ef660a53dd4bed12
James <james@rocketer.com>
2023-06-26 16:52:47 +0100

    Forced API version to 17.0

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -23,7 +23,7 @@
  */
 namespace FacebookAds;
 class ApiConfig {
-  const APIVersion = '15.0';
+  const APIVersion = '17.0';
   const SDKVersion = '15.0.0';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
